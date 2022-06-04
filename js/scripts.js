683de16c85ce14404c00edb86f4f53e177092f21
Kate_Style <e.ivanova.93@bk.ru>
2022-06-05 00:33:39 +0300

    burger-nav 35%

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let button = document.getElementById('header__nav-button');
+let navigation = document.getElementById('menu');
+button.addEventListener('click', function () {
+    navigation.classList.toggle('display-block');
+});
