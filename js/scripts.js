7860acebc25bc9835ce3c24d7bcc60ff1c1d8383
Katerine <e.ivanova.93@bk.ru>
2022-06-13 21:43:12 +0300

    Auto stash before merge of "main" and "origin/main"

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 let button = document.getElementById('header__nav-button');
 let navigation = document.getElementById('menu');
 button.addEventListener('click', function () {
     navigation.classList.toggle('display-block');
 });
+=======
+let button = document.getElementById('header__nav-button');
+let navigation = document.getElementById('menu');
+button.addEventListener('click', function () {
+    navigation.classList.toggle('display-block');
+});
+>>>>>>> Stashed changes
