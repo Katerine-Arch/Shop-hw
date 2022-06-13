f9bd9c3bbf54eed819cdec554402aeaee55e24a9
Katerine <e.ivanova.93@bk.ru>
2022-06-13 22:41:14 +0300

    upd

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
-<<<<<<< Updated upstream
 let button = document.getElementById('header__nav-button');
 let navigation = document.getElementById('menu');
 button.addEventListener('click', function () {
     navigation.classList.toggle('display-block');
 });
-=======
-let button = document.getElementById('header__nav-button');
-let navigation = document.getElementById('menu');
-button.addEventListener('click', function () {
-    navigation.classList.toggle('display-block');
-});
->>>>>>> Stashed changes
+
