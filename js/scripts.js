fefaa96f63430c7fb69cf03158d0b23168b00851
Kate_Style <e.ivanova.93@bk.ru>
2022-06-18 23:23:59 +0300

    +

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let button = document.getElementById('header__nav-button');
 let navigation = document.getElementById('menu');
+let wrp = document.getElementById('wrp');
 button.addEventListener('click', function () {
     navigation.classList.toggle('display-block');
     wrp.classList.toggle('display-block');
